33ef0c322091d82974f19645a638f71934dc8377
Pedro Fernandes <pedro.fernnandessz@gmail.com>
2025-10-26 15:24:39 -0300

    Adicionado query selector para o formulário

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar items do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+  const element = document.getElementById(parametro)
+  if(element){
+    element.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto)
